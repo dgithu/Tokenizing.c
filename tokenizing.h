0c57f9784a4e0b74b7c9b78b153808916a697d71
dgithu <111023350+dgithu@users.noreply.github.com>
2022-08-10 18:21:18 -0400

    Create tokenizing.h

---
+++ tokenizing.h
@@ -0,0 +1,7 @@
+//TOKENIZING MODULE HEADER
+#ifndef _TOKENIZING_H_ /*it will check if macro is defined or not and if it is not defined it will run the else statement*/
+#define _TOKENIZING_H_ /*defining macro tokenizing_h*/
+#include <stdio.h>  
+#include <string.h> /*we included the header file for using strings*/
+void tokenizing(void); /*function of tokenizing*/
+#endif
